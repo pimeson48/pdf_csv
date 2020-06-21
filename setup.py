c06c88df98df4dec972f2e6537f612cf08435e4e
Jon Chuma <jchuma@gmail.com>
2020-06-21 06:57:34 -0700

    1.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     keywords='pdf_csv',
     name='pdf_csv',
-    packages=find_packages(include=['pdf_csv', 'pdf_csv.*', 'defaults']),
+    packages=find_packages(include=['pdf_csv', 'pdf_csv.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
